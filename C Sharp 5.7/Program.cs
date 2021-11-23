be953ef7ad73868f755d638947619e8b60278fb3
SpinningHook18 <sy212775@xaverian.ac.uk>
2021-11-23 22:43:00 +0000

    Initial commit

---
+++ C Sharp 5.7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_5._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter a name:");
+            name = Console.ReadLine();
+            for (int i = 0; i < name.Length; i++)
+            {
+                Replace()
+            }
+        }
+    }
+}
